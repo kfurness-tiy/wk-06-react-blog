56cd7f0dc719cae43e02cb92b006da9ea4835a53
kelly <kelly.n.waite@gmail.com>
2016-11-01 17:54:06 -0600

    feat(core) Copy img to dist through scripts; Style header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -6,8 +6,8 @@ import headerStyle from './Header.sass';
 export class Header extends React.Component {
   render () {
     return (
-      <div className="header col-md-4">
-        <img src="../img/catlogo.png" />
+      <div className="header col-sm-12">
+        <img src="./app/img/catlogo.png" />
         <h1>Life of a Cat</h1>
       </div>
     );
