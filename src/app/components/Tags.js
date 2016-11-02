1825180a2a604c5a85ab55ff5309de1e92a95282
kelly <kelly.n.waite@gmail.com>
2016-11-02 14:27:34 -0600

    feat(core) Remove unneeded code

--- src/app/components/Tags.js
+++ src/app/components/Tags.js
@@ -2,14 +2,14 @@
 
 import React from 'react';
 
+
 export class Tags extends React.Component {
   render () {
     return (
       <ul>
-        <li>Tag 1</li>
         <li>Tag 2</li>
         <li>Tag 3</li>
       </ul>
-    )
+    );
   }
 }
