33b93a8de6d9771e6755602b6115079e1da1a301
kelly <kelly.n.waite@gmail.com>
2016-11-17 16:21:57 -0700

    WIP mapping through firebase data

--- src/app/components/Post.js
+++ src/app/components/Post.js
@@ -7,7 +7,10 @@ import PostTags from "./PostTags";
 
 export default class Post extends React.Component {
   render () {
-    
+    let dataSet = this.props.blogData;
+    this.props.blogData.on('child_added', (snapshot) => {
+  console.log('pizza',snapshot.val());
+});
     const blabla = this.props.blogData.child('0');
     blabla.on('value', (snapshot) => {
       //displays entire object
@@ -16,18 +19,21 @@ export default class Post extends React.Component {
     console.log('yo yo yo',blabla);
     return (
       <section>
-        {this.props.blogData.map(function(c,i,a) {
-          return (
-            <div key={"article", i} className="post col-sm-6">
-              <h2 key={"title", i}>{a[i].title}</h2>
-              <PostDate dateData={a[i].date} />
-              <PostParagraphs pData={a[i].article} />
-              <PostTags tagData={a[i].tags} />
-            </div>
-            );
-          })
-        }
+            {this.props.blogData.on('child_added', (snapshot) => {
+              <div className="post col-sm-6">
+                <h2>{dataSet.child('title')}</h2>
+                <PostDate dateData={dataSet.child('date')} />
+                <PostParagraphs pData={dataSet.child('article')} />
+                <PostTags tagData={dataSet.child('tags')} />
+              </div>
+            })}
       </section>
     );
   }
 }
+
+
+// bundle.js:9023 Uncaught Error: Objects are not valid as a React child (found: https://wk-06-react-blog.firebaseio.com/title). If you meant to render a collection of children, use an array instead or wrap the object using createFragment(object) from the React add-ons. Check the render method of `Post`.(…)
+
+//http://stackoverflow.com/questions/33726179/reactfire-get-objects-are-not-valid-as-a-react-child-when-binding-firebase-t
+//https://www.npmjs.com/package/react-addons-create-fragment
